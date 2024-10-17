fa7d8c384d0dce46d1cd8760ea2854e1190f802c
Angel Marin <133170788+Legna800@users.noreply.github.com>
2024-10-17 18:28:19 +0200

    Add files via upload

---
+++ prova1/HolaMeuNom.java
@@ -0,0 +1,14 @@
+package prova1;
+
+/**
+ * @author AngelMarin
+ */
+public class HolaMeuNom {
+    public static void main(String[] args) {
+        
+        String nombre = "Angel Marin Ascaso";
+
+        // Mostrar el mensaje en pantalla
+        System.out.println("Hola em dic " + nombre);
+    }
+}
