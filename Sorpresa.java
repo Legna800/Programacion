27993b9826fecd394515a7b9d0cd04cf1fa1cccc
Angel Marin <133170788+Legna800@users.noreply.github.com>
2024-10-25 08:54:51 +0200

    Sorpresa.java

---
+++ Sorpresa.java
@@ -0,0 +1,12 @@
+package en_proceso;
+public class Carpeta{
+public static void main(String[] args) {
+    int numUno = 3;
+    int numDos = 7;
+    if (numUno >= numDos) {
+        System.out.println(numUno);
+    }else{
+        System.out.println(numDos);
+    }
+}
+}
